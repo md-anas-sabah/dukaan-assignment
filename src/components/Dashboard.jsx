e9cdc386f7da9b2b27a8af910cff370d9f15c4fe
Md Anas Sabah <mdanassabah@gmail.com>
2024-01-10 14:32:14 +0530

    added tables

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,8 @@
 import Header from "./Header.jsx";
+import orderData from "../Data/data.json";
 
 function Dashboard() {
+  console.log(orderData);
   return (
     <div className="w-[1216px] bg-[#FAFAFA]">
       <Header />
@@ -69,23 +71,51 @@ function Dashboard() {
             {/* <div className="w-[1128px flex py-[10px] px-3 items-center gap-10 rounded bg-[#F2F2F2] mt-3"> */}
             <table className="w-[1128px] mt-3">
               {/* order Id order date */}
-              <thead className="flex flex-col py-[10px] px-3  gap-10 rounded bg-[#F2F2F2]">
-                <tr>
-                  <th className="flex items-start flex-col flex-1 ">Order ID</th>
-                  <th>
-                    Order date
-                    <img src="./images/triangle.svg" alt="down" />
+              <thead className="flex py-[10px] px-3  rounded bg-[#F2F2F2]">
+                <tr className="flex gap-10 w-full">
+                  <th className="flex items-start flex-col flex-1 font-px-regular text-[14px] font-medium leading-5">
+                    Order ID
                   </th>
-                  <th>Order amount</th>
-                  <th>
-                    Transaction fees <img src="./images/info.svg" alt="info" />
+                  <th className="flex flex-1 items-center gap-1  font-px-regular text-[14px] font-medium leading-5">
+                    Order date
+                    <img
+                      src="./images/triangle.svg"
+                      alt="down"
+                      className="h-2 w-2"
+                    />
+                  </th>
+                  <th className="flex flex-1 justify-end items-center gap-1  font-px-regular text-[14px] font-medium leading-5">
+                    Order amount
+                  </th>
+                  <th className="flex flex-1 justify-end items-center gap-1  font-px-regular text-[14px] font-medium leading-5">
+                    Transaction fees <img src="./images/Info.svg" alt="info" />
                   </th>
                 </tr>
               </thead>
-              <tbody></tbody>
+              {orderData.map((data) => {
+                return (
+                  <tbody
+                    className="flex py-[10px] px-3  rounded"
+                    key={data.orderId}
+                  >
+                    <tr className="flex gap-10 w-full">
+                      <td className="flex items-start flex-col flex-1 font-px-regular text-[14px] font-medium leading-5 text-[#146EB4] ">
+                        {data.orderId}
+                      </td>
+                      <td className="flex flex-1 items-center gap-1  font-px-regular text-[14px] font-normal leading-5">
+                        {data.orderDate}
+                      </td>
+                      <td className="flex flex-1 justify-end items-center gap-1  font-px-regular text-[14px] font-normal leading-5">
+                        {data.orderAmount}
+                      </td>
+                      <td className="flex flex-1 justify-end items-center gap-1  font-px-regular text-[14px] font-normal leading-5">
+                        {data.transaction}
+                      </td>
+                    </tr>
+                  </tbody>
+                );
+              })}
             </table>
-            {/* </div> */}
-            <div>{/*  */}</div>
           </div>
         </div>
       </div>
